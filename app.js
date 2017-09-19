8ac6c44337a654b4ac5fd1bb9b49c2c53934168e
Felix Gladisch <fg@in-telegence.net>
2017-09-19 17:12:42 +0200

    fix: cors headers

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(bodyParser.json())
 
 app.use((req, res, next) => {
   res.set('Access-Control-Allow-Origin', '*')
+  res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+  res.set('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization')
   return next()
 })
 
