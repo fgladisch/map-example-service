294095d826eb12f243bd05ea516349186a9931ac
Felix Gladisch <fg@in-telegence.net>
2017-09-21 11:37:24 +0200

    fix: error handling

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,12 +1,12 @@
 const ServerError = require('../util/server-error')
 
-function errorHandler(err, req, res) {
+function errorHandler(err, req, res, next) {
 
   if (err instanceof ServerError) {
     return res.status(err.status).send(err.message)
   }
 
-  res.status(500).send(err)
+  res.status(500).send(err.message)
 }
 
 module.exports = errorHandler
